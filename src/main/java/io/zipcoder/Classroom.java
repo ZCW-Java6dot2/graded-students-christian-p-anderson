93ec0cc04d2f54868d98b556f7f946d102d38891
christian <christian@zipcodes-MBP-51.lan>
2020-11-11 09:30:19 -0500

    completed additional methods

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,5 +1,8 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Classroom {
 
     private Student[] students;
@@ -30,5 +33,48 @@ public class Classroom {
         }
 
         return Math.round(sumOfAllStudentAverageExamScores / students.length);
+    }
+
+    /**
+     * use a for loop to create an array the size of the class and assign their reference to the array elements
+     * this allows us to add a student to the array. Check if the value at the element is null, if so this is the
+     * next empty spot in the array. Add the student to this empty spot. Return true if the method is successful and
+     * false if it's not successful
+     */
+    public Boolean addStudent(Student student) {
+        for (int i = 0; i < this.students.length; i++) {
+            if (this.students[i] == null) {
+                this.students[i] = student;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * convert array to an ArrayList. find if the student exists, if so store the student object so we can remove it
+     * from the ArrayList. remove student and then convert the ArrayList back to an array
+     */
+    public Boolean removeStudent(String firstName, String lastName) {
+       Student studentToBeRemoved = null;
+
+       for (Student student : this.students) {
+           if (student.getFirstName().equals(firstName) && student.getLastName().equals(lastName)) {
+               studentToBeRemoved = student;
+               break;
+           }
+       }
+
+       if (studentToBeRemoved == null) {
+           return false;
+       } else {
+           ArrayList<Student> studentsList = new ArrayList<>(Arrays.asList(this.students));
+           studentsList.remove(studentToBeRemoved);
+
+           this.students = studentsList.toArray(this.students);
+           return true;
+       }
+
+
     }
 }
