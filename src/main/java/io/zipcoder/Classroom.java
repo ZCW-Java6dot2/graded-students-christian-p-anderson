6aa74b37764dae1cf195002f301a9d13f71c8bf4
christian <christian@zipcodes-MBP-51.lan>
2020-11-10 21:59:11 -0500

    completed getAverageExamScore method in Classroom class

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,34 @@
 package io.zipcoder;
 
 public class Classroom {
+
+    private Student[] students;
+
+    public Classroom(int maxNumberOfStudents) {
+        this.students = new Student[maxNumberOfStudents];
+    }
+
+    public Classroom(Student[] students) {
+        this.students = students;
+    }
+
+    public Classroom() {
+        this.students = new Student[30];
+    }
+
+    public Student[] getStudents() {
+        return this.students;
+    }
+
+    public double getAverageExamScore() {
+        double sumOfAllStudentAverageExamScores = 0.00;
+
+        for (Student student : students) {
+            if (student != null) {
+                sumOfAllStudentAverageExamScores += student.getAverageExamScore();
+            }
+        }
+
+        return Math.round(sumOfAllStudentAverageExamScores / students.length);
+    }
 }
